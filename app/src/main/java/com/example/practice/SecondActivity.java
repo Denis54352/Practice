374664b209dcbbe85be20dab4ea7c24c5fb7da59
Merillax <artem-gusev-04@mail.ru>
2024-03-13 10:33:20 +0300

    Third commit of the three day

--- app/src/main/java/com/example/practice/SecondActivity.java
+++ app/src/main/java/com/example/practice/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,9 +14,13 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
     }
 
-    public void run() {
+    public void onClick(View view) {
+        Intent intent = new Intent(SecondActivity.this, FourthActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick2(View view) {
         Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
         startActivity(intent);
-        finish();
     }
 }
--- app/src/main/java/com/example/practice/ThirdActivity.java
+++ app/src/main/java/com/example/practice/ThirdActivity.java
@@ -2,7 +2,9 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(ThirdActivity.this, FourthActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick3(View view) {
+        Intent intent = new Intent(ThirdActivity.this, FourthActivity.class);
+        startActivity(intent);
     }
 }
